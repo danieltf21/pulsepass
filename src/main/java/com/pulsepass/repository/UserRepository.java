df6b472f06687945ce027f60ba66d12a81f48581
Juan2905-sudo <juanjo200529@hotmail.com>
2026-09-20 11:30:14 -0500

    FR-EVT/SRC/TKT: repositories

--- src/main/java/com/pulsepass/repository/UserRepository.java
+++ src/main/java/com/pulsepass/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.pulsepass.repository;
 
-public class UserRepository {
+import com.pulsepass.domain.User;
+import org.hibernate.internal.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailIgnoreCase(String email);
+    Optional<User> findByUsername(String username);
 }
